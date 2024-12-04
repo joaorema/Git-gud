cdc516003190efe4b5808bd23543537d7ac301b6
MarkosComK <markoscomks@gmail.com>
2024-12-04 16:34:24 +0000

    add: Added contributors folder

---
+++ contributors/marsoare.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   marsoare.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marsoare <marsoare@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/04 16:29:46 by marsoare          #+#    #+#             */
+/*   Updated: 2024/12/04 16:32:50 by marsoare         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/* 
+ * Hey, buddies. My name is Markos and i'm proud to make my very first workshop
+ * and presentation trying to teach and share a bit of my knowledge. The best
+ * way to learn is teaching!
+ *
+ * It's a pleasure be part of 42 and i just want to help the school to improve
+ * this project it's the way i found to contribute to the community.
+ *
+ * If you reached that line, thanks to read that. No A.I was used, every word
+ * came from the hearth. <3
+ *
+ */
