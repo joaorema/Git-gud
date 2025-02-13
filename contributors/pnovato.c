b9a325ba387f75901c1e13ba8695715dc34b6e4f
pnovato <ptrck.nvt.slv@gmail.com>
2025-02-13 21:58:38 +0000

    adding pnovato.c

---
+++ contributors/pnovato.c
@@ -0,0 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pnovato.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pnovato- <pnovato-@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/13 21:47:12 by pnovato-          #+#    #+#             */
+/*   Updated: 2024/10/30 21:48:07 by pnovato-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+/*
+* Hi everyone! I'm Patrick aka pnovato- n I'm happy to be part of this great effort
+* started by marsoare and hluiz-ma. 
+* N always remenber ... to GIT PUSH!!
+*/
